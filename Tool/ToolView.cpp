1c7355c2bdc22d3240d20e454a292ca48ca9d063
marujoy01 <46582908+marujoy01@users.noreply.github.com>
2023-08-01 17:16:22 +0900

    5일차

---
+++ Tool/ToolView.cpp
@@ -0,0 +1,127 @@
+
+// ToolView.cpp : CToolView 클래스의 구현
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "Tool.h"
+#endif
+
+#include "ToolDoc.h"
+#include "ToolView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+HWND	g_hWnd;
+
+// CToolView
+
+IMPLEMENT_DYNCREATE(CToolView, CView)
+
+BEGIN_MESSAGE_MAP(CToolView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+	ON_WM_DESTROY()
+END_MESSAGE_MAP()
+
+// CToolView 생성/소멸
+
+CToolView::CToolView()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CToolView::~CToolView()
+{
+}
+
+#pragma region 생략
+
+BOOL CToolView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CToolView 인쇄
+
+BOOL CToolView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CToolView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CToolView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CToolView 진단
+
+#ifdef _DEBUG
+void CToolView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CToolView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CToolDoc* CToolView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CToolDoc)));
+	return (CToolDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CToolView 메시지 처리기
+
+#pragma endregion 생략
+
+void CToolView::OnInitialUpdate()
+{
+	CView::OnInitialUpdate();
+
+	g_hWnd = m_hWnd;
+
+	if (FAILED(CDevice::Get_Instance()->Initialize()))
+		AfxMessageBox(L"CDevice Initialize FAILED");
+}
+
+void CToolView::OnDraw(CDC* /*pDC*/)
+{
+	CToolDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	CDevice::Get_Instance()->Render_Begin();
+
+
+	CDevice::Get_Instance()->Render_End();
+}
+
+void CToolView::OnDestroy()
+{
+	CView::OnDestroy();
+
+	CDevice::Get_Instance()->Destroy_Instance();
+}
